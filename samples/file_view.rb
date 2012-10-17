4409961591caa32ab411e66af68e002a21661dbb
Thomas E. Enebo <tom.enebo@gmail.com>
2012-10-17 11:00:22 -0500

    Minimize already tiny sample...comment was not helpful

--- samples/file_view.rb
+++ samples/file_view.rb
@@ -1,6 +1,3 @@
 require 'image_voodoo'
 
-# preview the file specified by ARGV[0] in a swing window
-ImageVoodoo.with_image(ARGV[0]) do |img|
-  img.preview
-end
+ImageVoodoo.with_image(ARGV[0]) { |img| img.preview }
