7711aa917c64b04b8de9cef5666926dc53bbd8e6
Thomas E. Enebo <tom.enebo@gmail.com>
2015-02-13 10:20:13 -0600

    Make test_metadata.rb an optional run for now

--- test/test_metadata.rb
+++ test/test_metadata.rb
@@ -8,43 +8,43 @@ IMAGE_DIR = File.join File.dirname(__FILE__), '..', '..', 'metadata-extractor-im
 # subdir to this project and probably clone repo into that subdir
 unless File.exist? IMAGE_DIR
   puts "To run this test you must clone: https://github.com/drewnoakes/metadata-extractor-images.git into a sibling directory to image_voodoo"
-end
-
-class TestImageVoodooMetadata < Test::Unit::TestCase
-  def setup
-    @path = File.join IMAGE_DIR, "Apple iPhone 4S.jpg"
-    @path_gps = File.join IMAGE_DIR, "Apple iPhone 4.jpg"
-    @path_no_exif = File.join File.dirname(__FILE__), "pix.png"
-  end
-  def test_metadata_from_file
-    ImageVoodoo.with_image @path do |img|
-      assert img.metadata[:IFD0].exists?
-      assert_equal 6, img.metadata[:IFD0][:Orientation]
-      assert_equal 6, img.metadata.orientation
-      assert_equal 3264, img.metadata.width
-      assert_equal 2448, img.metadata.height
-      assert_equal "Apple", img.metadata.make
-      assert_equal "iPhone 4S", img.metadata.model
+else
+  class TestImageVoodooMetadata < Test::Unit::TestCase
+    def setup
+      @path = File.join IMAGE_DIR, "Apple iPhone 4S.jpg"
+      @path_gps = File.join IMAGE_DIR, "Apple iPhone 4.jpg"
+      @path_no_exif = File.join File.dirname(__FILE__), "pix.png"
     end
-  end
-
-  def test_metadata_from_inputstream
-    ImageVoodoo.with_bytes File.read(@path) do |img|
-      assert_equal(6, img.metadata[:IFD0][:Orientation])
+    def test_metadata_from_file
+      ImageVoodoo.with_image @path do |img|
+        assert img.metadata[:IFD0].exists?
+        assert_equal 6, img.metadata[:IFD0][:Orientation]
+        assert_equal 6, img.metadata.orientation
+        assert_equal 3264, img.metadata.width
+        assert_equal 2448, img.metadata.height
+        assert_equal "Apple", img.metadata.make
+        assert_equal "iPhone 4S", img.metadata.model
+      end
     end
-  end
 
-  def test_metadata_no_ifd0
-    ImageVoodoo.with_image @path_no_exif do |img|
-      assert !img.metadata[:IFD0].exists?
-      assert_equal(nil, img.metadata[:IFD0][:Orientation])
+    def test_metadata_from_inputstream
+      ImageVoodoo.with_bytes File.read(@path) do |img|
+        assert_equal(6, img.metadata[:IFD0][:Orientation])
+      end
     end
-  end
 
-  def test_metadata_gps
-    ImageVoodoo.with_image @path_gps do |img|
-      assert img.metadata[:Gps].exists?
-      assert_equal("N", img.metadata[:Gps]['Latitude Ref'])
+    def test_metadata_no_ifd0
+      ImageVoodoo.with_image @path_no_exif do |img|
+        assert !img.metadata[:IFD0].exists?
+        assert_equal(nil, img.metadata[:IFD0][:Orientation])
+      end
+    end
+
+    def test_metadata_gps
+      ImageVoodoo.with_image @path_gps do |img|
+        assert img.metadata[:Gps].exists?
+        assert_equal("N", img.metadata[:Gps]['Latitude Ref'])
+      end
     end
   end
 end
