8a4e0cdc832c139bb669507e39f68650ed44ea6f
Thomas E. Enebo <tom.enebo@gmail.com>
2017-04-18 16:55:23 -0500

    Test vertical and horizontal transforms

--- test/test_image_voodoo.rb
+++ test/test_image_voodoo.rb
@@ -28,16 +28,29 @@ class TestImageVoodoo < Test::Unit::TestCase
   # TODO: Don't fully know how to predict pixel values for adjust_brightness.
 
   def test_alpha
-    image = make_square
-    image = image.alpha('000000')
+    image = make_square.alpha('000000')
     assert_equal(0xff, image.color_at(*upper_left).alpha)
+  end
+
+  # 04 -> 40
+  # 8b    b8
+  def test_flip_horizontally
+    image = make_square.flip_horizontally
+    assert_color(image.color_at(*upper_left), 0x44, 0x44, 0x44)
+  end
+
+  # 04 -> 8b
+  # 8b    04
+  def test_flip_vertically
+    image = make_square.flip_vertically
+    assert_color(image.color_at(*upper_left), 0x88, 0x88, 0x88)
   end
 
   # 04 -0-> 04 -90-> 80 -180-> 4b -270-> b8
   # 8b      8b       b4        08        40
   def test_rotate
     image = make_square
-    assert_color(image.color_at(*upper_left), 0x0, 0x0, 0x00)
+    assert_color(image.color_at(*upper_left), 0x00, 0x00, 0x00)
     image = image.rotate(90)
     assert_color(image.color_at(*upper_left), 0x88, 0x88, 0x88)
     image = image.rotate(180)
